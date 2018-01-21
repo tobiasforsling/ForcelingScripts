db79d58bacd8450c0b131c95790160e99ac65d23
Tobias Forsling <tobban94@gmail.com>
2018-01-21 23:57:44 +0100

    created control panel script, basic enable disable for gameobject1

---
+++ Assets/Editor/ForcelingControlPanelEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ForcelingControlPanel))]
+public class ForcelingControlPanelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ForcelingControlPanel myscript = (ForcelingControlPanel)target;
+
+        DrawDefaultInspector();
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Enable", GUILayout.Height(40), GUILayout.Width(120)))
+        {
+            myscript.Enable();
+        }
+
+        if (GUILayout.Button("Disable", GUILayout.Height(40), GUILayout.Width(120)))
+        {
+            myscript.Disable();
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Enable Post Processing", GUILayout.Height(40), GUILayout.Width(200)))
+        {
+            myscript.EnablePostProcess();
+        }
+
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+    }
+}
---
+++ Assets/ForcelingControlPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class ForcelingControlPanel : MonoBehaviour{
+    public GameObject[] NumberOfObjects;
+    public static bool ObjActivity;
+    public Camera MainCamera;
+
+    public void Enable()
+    {
+        NumberOfObjects[1].SetActive(true);
+    }
+
+    public void Disable()
+    {
+        NumberOfObjects[1].SetActive(false);
+    }
+
+    public void EnablePostProcess()
+    {
+        //MainCamera.enabled = true;
+    }
+
+    public void DisablePostProcess()
+    {
+        //MainCamera.enabled = true;
+    }
+}
