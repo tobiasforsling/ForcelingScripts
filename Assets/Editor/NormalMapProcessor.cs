b20232a5f0c2d1dc813678412ff01813a4c67ec1
Tobias Forsling <tobban94@gmail.com>
2018-02-28 13:04:05 +0100

    created normal map processor for .tga files

---
+++ Assets/Editor/NormalMapProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NormalMapProcessor : AssetPostprocessor {
+
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.tga") != -1;
+
+        if (hasNormalID)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureType = TextureImporterType.NormalMap;
+        }
+    }
+}
