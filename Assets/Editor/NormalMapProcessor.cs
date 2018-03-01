51669aa1cb474ea72784760a593e5501184c9ee4
Tobias Forsling <tobban94@gmail.com>
2018-03-01 23:30:38 +0100

    changed normalmap processor to not be .tga specific. started creation of skybox processor but bool doesnt seem to return true. aka its not working

--- Assets/Editor/NormalMapProcessor.cs
+++ Assets/Editor/NormalMapProcessor.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class NormalMapProcessor : AssetPostprocessor {
+public class NormalMapProcessor : AssetPostprocessor
+{
 
     void OnPostprocessTexture(Texture2D texture)
     {
         string lowerCaseAssetPath = assetPath.ToLower();
-        bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.tga") != -1;
+        bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.") != -1;
 
         if (hasNormalID)
         {
+            Debug.Log("_n. is found in filepath");
             TextureImporter textureImporter = (TextureImporter)assetImporter;
             textureImporter.textureType = TextureImporterType.NormalMap;
         }
