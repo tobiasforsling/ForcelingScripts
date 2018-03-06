e17a7e54247b54cb0ef98bbc926fe6cf0918b597
Tobias Forsling <tobban94@gmail.com>
2018-03-06 01:10:01 +0100

    added texture format change to normal map processor, added .fbx check

--- Assets/Editor/ImportProcessor.cs
+++ Assets/Editor/ImportProcessor.cs
@@ -5,44 +5,46 @@ using UnityEditor;
 
 public class ImportProcessor : AssetPostprocessor
 {
-    /*
-    string lowerCaseAssetPath = assetPath.ToLower();
+    
 
-    void OnPostprocessModel()
+    
+    void OnPreprocessModel(GameObject model)
     {
-
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool hasFbxID = lowerCaseAssetPath.IndexOf(".fbx") != -1;
 
         if (hasFbxID)
         {
-            //Debug.Log(".fbx is found in filepath");
-            ModelImporter modelimporter = (ModelImporter)assetImporter;
-            ModelImporter.mat = TextureImporterType.Default;
-            textureImporter.textureShape = TextureImporterShape.TextureCube;
-            textureImporter.wrapMode = TextureWrapMode.Repeat;
+            Debug.Log(".fbx is found in filepath");
+            //ModelImporter modelImporter = (ModelImporter)assetImporter;
+            //modelImporter.ModelImporterMaterialLocation = ModelImporterMaterialLocation.External;
         }
     }
-    */
 
     void OnPostprocessTexture(Texture2D texture)
     {
-        bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.") != -1;
+        string lowerCaseAssetPath = assetPath.ToLower();
         bool hasSkyID = lowerCaseAssetPath.IndexOf("sky_") != -1;
-        bool hasFbxID = lowerCaseAssetPath.IndexOf(".fbx1") != -1;
+        bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.") != -1;
 
         if (hasSkyID)
         {
-            //Debug.Log("Sky_ is found in filepath");
+            Debug.Log("Sky_ is found in filepath");
             TextureImporter textureImporter = (TextureImporter)assetImporter;
             textureImporter.textureType = TextureImporterType.Default;
             textureImporter.textureShape = TextureImporterShape.TextureCube;
             textureImporter.wrapMode = TextureWrapMode.Repeat;
         }
 
+
         if (hasNormalID)
         {
-            //Debug.Log("_n. is found in filepath");
+            Debug.Log("_n. is found in filepath");
             TextureImporter textureImporter = (TextureImporter)assetImporter;
             textureImporter.textureType = TextureImporterType.NormalMap;
+            //textureImporter.textureFormat = TextureImporterFormat.DXT5;
+            //AssetImporter.SaveAndReimport; //Needs save and reimport to apply correct compression
+            textureImporter.textureCompression = TextureImporterCompression.Compressed;
         }
 
 
