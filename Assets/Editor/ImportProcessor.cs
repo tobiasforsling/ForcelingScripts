2ce13affe8a7af18e44629aa1b6e5965f8b45317
Tobias Forsling <tobban94@gmail.com>
2018-03-08 15:40:23 +0100

    Import processor now converts normal maps, sky maps and fbx files

--- Assets/Editor/ImportProcessor.cs
+++ Assets/Editor/ImportProcessor.cs
@@ -5,48 +5,44 @@ using UnityEditor;
 
 public class ImportProcessor : AssetPostprocessor
 {
-    
-
-    
-    void OnPreprocessModel(GameObject model)
+    void OnPreprocessModel()
     {
         string lowerCaseAssetPath = assetPath.ToLower();
         bool hasFbxID = lowerCaseAssetPath.IndexOf(".fbx") != -1;
 
+        ModelImporter FbxImporter = (ModelImporter)assetImporter;
+        
+        //IMPORTING .FBX FILES WITH LEGACY MATERIAL METHOD
         if (hasFbxID)
         {
-            Debug.Log(".fbx is found in filepath");
-            //ModelImporter modelImporter = (ModelImporter)assetImporter;
-            //modelImporter.ModelImporterMaterialLocation = ModelImporterMaterialLocation.External;
+            //Debug.Log(".fbx is found in filepath");
+            FbxImporter.materialLocation = ModelImporterMaterialLocation.External;
         }
     }
 
-    void OnPostprocessTexture(Texture2D texture)
+    void OnPreprocessTexture()
     {
         string lowerCaseAssetPath = assetPath.ToLower();
-        bool hasSkyID = lowerCaseAssetPath.IndexOf("sky_") != -1;
         bool hasNormalID = lowerCaseAssetPath.IndexOf("_n.") != -1;
+        bool hasSkyID = lowerCaseAssetPath.IndexOf("sky_") != -1;
 
+        TextureImporter NormalImporter = (TextureImporter)assetImporter;
+        TextureImporter SkyImporter = (TextureImporter)assetImporter;
+
+        //IMPORTING "_N." AS NORMAL MAPS
+        if (hasNormalID && NormalImporter.textureType != TextureImporterType.NormalMap)
+        {
+            NormalImporter.textureType = TextureImporterType.NormalMap;
+            //Debug.Log("_n. is found in filepath, file type changed");
+        }
+
+        //IMPORTING "SKY_" FILES AS CUBEMAPS WITH REPEATING WRAPMODE
         if (hasSkyID)
         {
-            Debug.Log("Sky_ is found in filepath");
-            TextureImporter textureImporter = (TextureImporter)assetImporter;
-            textureImporter.textureType = TextureImporterType.Default;
-            textureImporter.textureShape = TextureImporterShape.TextureCube;
-            textureImporter.wrapMode = TextureWrapMode.Repeat;
+            //Debug.Log("Sky_ is found in filepath, file type and shape changed");
+            SkyImporter.textureType = TextureImporterType.Default;
+            SkyImporter.textureShape = TextureImporterShape.TextureCube;
+            SkyImporter.wrapMode = TextureWrapMode.Repeat;
         }
-
-
-        if (hasNormalID)
-        {
-            Debug.Log("_n. is found in filepath");
-            TextureImporter textureImporter = (TextureImporter)assetImporter;
-            textureImporter.textureType = TextureImporterType.NormalMap;
-            //textureImporter.textureFormat = TextureImporterFormat.DXT5;
-            //AssetImporter.SaveAndReimport; //Needs save and reimport to apply correct compression
-            textureImporter.textureCompression = TextureImporterCompression.Compressed;
-        }
-
-
     }
 }
